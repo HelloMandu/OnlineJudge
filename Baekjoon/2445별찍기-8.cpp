2f03144c3ef1d864a153026d0297a7de08fbd4d8
Sungmin Jo <tjdals6695@gmail.com>
2019-02-08 16:11:59 +0900

    Baekjoon

--- Baekjoon/2445별찍기-8.cpp
+++ Baekjoon/2445별찍기-8.cpp
@@ -19,13 +19,13 @@ int main() {
 		cout << '\n';
 	}
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n - i - 1; j++) {
+		for (int j = 0; j < n - (i + 1); j++) {
 			cout << '*';
 		}
-		for (int k = 0; k < 2 * i + 2; k++) {
+		for (int k = 0; k < 2 * (i + 1); k++) {
 			cout << ' ';
 		}
-		for (int l = 0; l < n - i - 1; l++) {
+		for (int l = 0; l < n - (i + 1); l++) {
 			cout << '*';
 		}
 		cout << '\n';
