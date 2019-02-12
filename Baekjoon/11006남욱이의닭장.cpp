31a84643a8b07215d6ac9ad81210899fd58108b6
Sungmin Jo <tjdals6695@gmail.com>
2019-02-12 16:47:50 +0900

    Baekjoon

--- Baekjoon/11006남욱이의닭장.cpp
+++ Baekjoon/11006남욱이의닭장.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
-#define fastIO() ios_base::sync_base_studio(false), cin.tie(NULL), cout.tie(NULL);
+#define fastIO() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 using namespace std;
 
 int main() {
+	fastIO();
 	int testcase;
 	cin >> testcase;
 	while (testcase--) {
