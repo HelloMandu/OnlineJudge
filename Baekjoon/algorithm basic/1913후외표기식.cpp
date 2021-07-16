c62029bb7c2e77d07978929cfd91c9f3a852c338
HelloMandu <tjdals6695@gmail.com>
2021-07-16 22:32:01 +0900

    후위표기식

---
+++ Baekjoon/algorithm basic/1913후외표기식.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+bool isOperator(char c){
+    return c == '*' || c == '/' || c == '+' || c == '-';
+}
+
+double myOperator(char oper, double n, double m){
+    if(oper == '*'){
+        return n * m;
+    } else if(oper == '/'){
+        return n / m;
+    } else if(oper == '+'){
+        return n + m;
+    }
+    return n - m;
+}
+
+int arr[27];
+
+int main(){
+    freopen("input.txt", "r", stdin);
+    int n;
+    string str;
+    stack<double> Stack;
+    cin >> n >> str;˝
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int cur = 0;
+    for(int i = 0; i < str.size(); i++){
+        if(isOperator(str[i])){
+            double second = Stack.top(); Stack.pop();
+            double first = Stack.top(); Stack.pop();
+            Stack.push(myOperator(str[i], first, second));
+        } else{
+            Stack.push(arr[str[i] - 'A']);
+        }
+    }
+    printf("%.2lf", Stack.top());
+    return 0;
+}
