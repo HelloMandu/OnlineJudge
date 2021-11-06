68d3c60127bf1ff2aeef2f272541caa15d1c7c94
HelloMandu <tjdals6695@gmail.com>
2021-11-06 20:11:54 +0900

    unstaged file commit

--- Baekjoon/1157단어공부.cpp
+++ Baekjoon/1157단어공부.cpp
@@ -1,30 +1,27 @@
-#include<stdio.h>
-#include<string.h>
-#pragma warning(disable :4996)
+#include<bits/stdc++.h>
+using namespace std;
 
-char voca[1000000];
-int alphbet[26];
+int alphabet[26];
 
 int main() {
-	int n, max = 0;
+	int n, maxCnt = 0;
 	char result;
-	scanf("%s", &voca);
-
-	for (int i = 0; voca[i] != NULL; i++) {
-		if ('a' <= voca[i] && voca[i] <= 'z') // 소문자를 숫자로
-			n = voca[i] - 'a';
-		else if ('A' <= voca[i] && voca[i] <= 'Z') // 대문자를 숫자로
-			n = voca[i] - 'A';
-
-		alphbet[n]++; // 해당위치 +1
-
-		if (max < alphbet[n]) { // 갱신될때
-			max = alphbet[n];
-			result = n + 'A';
-		}
-		else if (max == alphbet[n]) // 같을때
-			result = '?';
+    string str;
+    cin >> str;
+	for(int i = 0; i < str.length(); i++) {
+		if ('a' <= str[i] && str[i] <= 'z'){
+			alphabet[str[i] - 'a']++;
+        }
+		else if ('A' <= str[i] && str[i] <= 'Z'){
+			alphabet[str[i] - 'A']++;
+        }
 	}
-	printf("%c\n", result);
+    for(int i = 0; i < 26; i++){
+        if(maxCnt <= alphabet[i]){
+            result = maxCnt == alphabet[i] ? '?' : i + 'A';
+            maxCnt = alphabet[i];
+        }
+    }
+    cout << result;
 	return 0;
 }
--- Baekjoon/9012괄호.cpp
+++ Baekjoon/9012괄호.cpp
@@ -3,32 +3,28 @@
 #include<string>
 using namespace std;
 
-bool VPS_Check(string &str) {
+bool isVps(string &str) {
 	stack<char> Stack;
 	for (int i = 0; i < str.length(); i++) {
-		if(str[i] == '(') // ( 일때
+		if(str[i] == '('){
 			Stack.push(str[i]);
-		else { // ) 일때
-			if (Stack.empty()) // 스택이 비어잇으면 괄호가 안맞으므로 false
-				return false;
-			Stack.pop(); // ( 와 ) 가 맞으므로 pop
+		}
+		else if(Stack.empty()){
+			return false;
+		} else{
+			Stack.pop();
 		}
 	}
-	return Stack.empty() ? true : false; // 스택이 비어잇을시 true
+	return Stack.empty();
 }
 
 int main() {
-	
 	int n;
 	string str;
 	cin >> n;
 	while (n--) {
 		cin >> str;
-		if (VPS_Check(str))
-			cout << "YES\n";
-		else
-			cout << "NO\n";
+		cout << (isVps(str) ? "YES\n" : "NO\n");
 	}
-
 	return 0;
 }
